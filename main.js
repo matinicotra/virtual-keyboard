6090ec70f6db4c1202195935cbb3778f1854308f
Matias Nicotra <matiasanicotra@gmail.com>
2024-05-01 17:30:51 -0300

    Ajustes

---
+++ main.js
@@ -0,0 +1,117 @@
+const keys = [
+  [
+    ["`", "˜"],
+    ["1", "!"],
+    ["2", "@"],
+    ["3", "#"],
+    ["4", "$"],
+    ["5", "%"],
+    ["6", "ˆ"],
+    ["7", "&"],
+    ["8", "*"],
+    ["9", "("],
+    ["0", ")"],
+    ["-", "_"],
+    ["=", "+"],
+  ],
+  [
+    ["q", "Q"],
+    ["w", "W"],
+    ["e", "E"],
+    ["r", "R"],
+    ["t", "T"],
+    ["y", "Y"],
+    ["u", "U"],
+    ["i", "I"],
+    ["o", "O"],
+    ["p", "P"],
+    ["p", "P"],
+    ["[", "]"],
+    ["{", "}"],
+  ],
+  [
+    ["MAYUS", "MAYUS"],
+    ["a", "A"],
+    ["s", "S"],
+    ["d", "D"],
+    ["f", "F"],
+    ["g", "G"],
+    ["h", "H"],
+    ["j", "J"],
+    ["k", "K"],
+    ["l", "L"],
+    [";", ":"],
+    ["'", ""],
+  ],
+  [
+    ["SHIFT", "SHIFT"],
+    ["z", "Z"],
+    ["x", "X"],
+    ["c", "C"],
+    ["v", "V"],
+    ["b", "B"],
+    ["n", "N"],
+    [",", "<"],
+    [".", ">"],
+    ["/", "?"],
+  ],
+  [["SPACE", "SPACE"]],
+];
+
+let mayus = false;
+let shift = false;
+let current = null;
+
+renderKeyboard();
+
+function renderKeyboard() {
+  const keyboardContainer = document.querySelector("#keyboard-container");
+  let empty = `<div class="key-empty"></div>`;
+
+  const layers = keys.map((layer) => {
+    return layer.map((key) => {
+      if (key[0] === "SHIFT") {
+        return `<button class="key key-shift>${key[0]}</button>`;
+      }
+
+      if (key[0] === "MAYUS") {
+        return `<button class="key key-mayus>${key[0]}</button>`;
+      }
+
+      if (key[0] === "SPACE") {
+        return `<button class="key key-space></button>`;
+      }
+
+      return `
+        <button class="key key-normal">
+        ${
+          shift
+            ? key[1]
+            : mayus &&
+              key[0].toLowerCase().charCodeAt(0) >= 97 &&
+              key[0].toLowerCase <= 122
+            ? key[1]
+            : key[0]
+        }
+        </button>
+      `;
+
+      layers[0].push(empty);
+      layers[1].unshift(empty);
+    });
+  });
+
+  layers[0].push(empty);
+  layers[1].unshift(empty);
+
+  const htmlLayers = layers.map((layer) => {
+    return layer.join("");
+  });
+
+  keyboardContainer.innerHTML = "";
+
+  htmlLayers.forEach((layer) => {
+    keyboardContainer.innerHTML += `<div class="layer">${layer}</div>`;
+  });
+}
+
