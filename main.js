3f57f7a76a5bba5cbb65ab88e6ef0666022ef21a
Matias Nicotra <matiasanicotra@gmail.com>
2024-05-01 20:17:07 -0300

    act

--- main.js
+++ main.js
@@ -58,8 +58,8 @@ const keys = [
   [["SPACE", "SPACE"]],
 ];
 
-let mayus = false;
 let shift = false;
+let mayus = false;
 let current = null;
 
 renderKeyboard();
@@ -67,19 +67,18 @@ renderKeyboard();
 function renderKeyboard() {
   const keyboardContainer = document.querySelector("#keyboard-container");
   let empty = `<div class="key-empty"></div>`;
-
   const layers = keys.map((layer) => {
     return layer.map((key) => {
       if (key[0] === "SHIFT") {
-        return `<button class="key key-shift>${key[0]}</button>`;
+        return `<button class="key key-shift">${key[0]}</button>`;
       }
 
       if (key[0] === "MAYUS") {
-        return `<button class="key key-mayus>${key[0]}</button>`;
+        return `<button class="key key-mayus ${mayus ? "activated" : ""}">${key[0]}</button>`;
       }
 
       if (key[0] === "SPACE") {
-        return `<button class="key key-space></button>`;
+        return `<button class="key key-space"></button>`;
       }
 
       return `
@@ -96,8 +95,6 @@ function renderKeyboard() {
         </button>
       `;
 
-      layers[0].push(empty);
-      layers[1].unshift(empty);
     });
   });
 
@@ -113,5 +110,35 @@ function renderKeyboard() {
   htmlLayers.forEach((layer) => {
     keyboardContainer.innerHTML += `<div class="layer">${layer}</div>`;
   });
+
+  document.querySelectorAll('.key').forEach(key => {
+    key.addEventListener('click', (e) => {
+      if (current) {
+        if (key.textContent === "SHIFT") {
+          shift = !shift;
+        } 
+        else if (key.textContent === "MAYUS") {
+          mayus = !mayus;
+        } 
+        else if (key.textContent === "") {
+          debugger;
+          current.value += " ";
+        } 
+        else {
+          current.value += key.textContent.trim();
+          if (shift) {
+            shift = false;
+          }
+        }
+        renderKeyboard();
+        current.focus();
+      }
+    });
+  })
 }
 
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener('focusin', (e) => {
+    current = e.target;
+  });
+});
